cee886365022acc3b5b1d28219426de442638118
giorgios17 <giosur@live.it>
2022-03-18 15:13:48 +0100

    create timer and input

---
+++ js/script.js
@@ -0,0 +1,65 @@
+//Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+//Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+//Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+
+const numbers = 5;
+const numbersToGuess = [];
+const num = document.querySelector('.numbers');
+const timer = document.querySelector('.timer');
+const myTimerInterval = setInterval(myCountdown, 1000);
+const numeriDigitati = [];
+const inputContainer = document.querySelector('.input-container');
+let countdown = 5;
+
+//numero random
+function numeroRandomRange(min, max){
+    const range = (max - min) + 1;
+    const numeroRandom = Math.floor(Math.random()*range + min);
+    return numeroRandom; 
+}
+
+//numer random unico
+function numeroUnico (min, max, used){
+    let numeroUnico = numeroRandomRange(min, max);
+    while(used.includes(numeroUnico)){
+        numeroUnico = numeroRandomRange(min, max);
+    }
+    return numeroUnico;
+}
+
+//5 numeri casuali da indovinare
+for(let i=0; i < numbers; i++){
+    let number = numeroUnico(1, 100, numbersToGuess)
+    numbersToGuess.push(number)
+}
+console.log(numbersToGuess);
+
+let showNumber = showNumberToGuess();
+
+function showNumberToGuess(){
+    for(let i=0; i < numbers; i++){
+        num.innerHTML = num.innerHTML + ' ' + numbersToGuess[i];
+    
+    }
+}
+
+
+function myCountdown (){
+    countdown--;
+    timer.innerHTML = 'Timer: ' + countdown;
+    if(countdown <= 0){
+        num.innerHTML= '';
+        inputContainer.classList.remove('d-none');
+        clearInterval(myTimerInterval);
+    }
+}
+
+// for(let i=0; i < numbers; i++){
+//     let numeroDigitato = parseInt(prompt('Digita un numero alla volta di quelli che hai memorizzato'))
+//     numeriDigitati.push(numeroDigitato);
+// }
+// console.log(numeriDigitati)
+
+
+
